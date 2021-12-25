e54d3881a220defb546a3cd7f0707c76c2fa5a5f
TORCHIZM <ismailpvo1535@gmail.com>
2021-12-25 17:14:25 +0300

    fix type

--- app/models/Item.js
+++ app/models/Item.js
@@ -11,7 +11,7 @@ const Session = sequelize.define("items", {
     },
     buyer: DataTypes.INTEGER,
 	qrCode: DataTypes.STRING,
-    redirectTo: DataTypes.STRING
+    redirectTo: DataTypes.TEXT
 });
 
 module.exports = Session;
